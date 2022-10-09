50250803a7a860eb36d90a2642729cb53552b7fd
YasinVeliyev <veliyev.yasin@gmail.com>
2022-10-09 22:45:50 +0400

    modified:   .gitignore 	new file:   rust/binary-search/.exercism/config.json 	new file:   rust/binary-search/.exercism/metadata.json 	new file:   rust/binary-search/.gitignore 	new file:   rust/binary-search/Cargo.toml 	new file:   rust/binary-search/HELP.md 	new file:   rust/binary-search/README.md 	new file:   rust/binary-search/src/lib.rs 	new file:   rust/binary-search/tests/binary-search.rs 	modified:   rust/high-scores/src/lib.rs 	new file:   typescript/two-fer/.eslintignore 	new file:   typescript/two-fer/.eslintrc.cjs 	new file:   typescript/two-fer/.exercism/config.json 	new file:   typescript/two-fer/.exercism/metadata.json 	new file:   typescript/two-fer/HELP.md 	new file:   typescript/two-fer/README.md 	new file:   typescript/two-fer/babel.config.cjs 	new file:   typescript/two-fer/jest.config.cjs

--- rust/high-scores/src/lib.rs
+++ rust/high-scores/src/lib.rs
@@ -1,27 +1,40 @@
 #[derive(Debug)]
-pub struct HighScores;
+pub struct HighScores<'a> {
+    all_scores: &'a [u32],
+}
 
-impl HighScores {
-    pub fn new(scores: &[u32]) -> Self {
-        unimplemented!(
-            "Construct a HighScores struct, given the scores: {:?}",
-            scores
-        )
+impl<'a> HighScores<'a> {
+    pub fn new(scores: &'a [u32]) -> Self {
+        Self { all_scores: scores }
     }
 
-    pub fn scores(&self) -> &[u32] {
-        unimplemented!("Return all the scores as a slice")
+    pub fn scores(&self) -> &'a [u32] {
+        self.all_scores
     }
 
     pub fn latest(&self) -> Option<u32> {
-        todo!()
+        self.all_scores.to_vec().pop()
     }
 
     pub fn personal_best(&self) -> Option<u32> {
-        unimplemented!("Return the highest score")
+        match self.all_scores.into_iter().max() {
+            Some(score) => Some(*score),
+            None => None,
+        }
     }
 
     pub fn personal_top_three(&self) -> Vec<u32> {
-        unimplemented!("Return 3 highest scores")
+        let mut vec = self.all_scores.to_vec();
+        vec.sort_by(|a, b| b.cmp(a));
+        // let mut v = Vec::<u32>::new();
+        // while v.len() < 3 {
+        //     match vec.pop() {
+        //         Some(score) => v.push(score),
+        //         None => return v,
+        //     }
+        // }
+        // v
+        vec.truncate(3);
+        vec
     }
 }
