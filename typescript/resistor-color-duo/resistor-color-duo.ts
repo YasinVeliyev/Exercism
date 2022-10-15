14339d107656bd1de9f201a0ad378545f27976a4
YasinVeliyev <veliyev.yasin@gmail.com>
2022-10-16 00:56:01 +0400

    Resistor Color

--- typescript/resistor-color-duo/resistor-color-duo.ts
+++ typescript/resistor-color-duo/resistor-color-duo.ts
@@ -1,6 +1,21 @@
+let Colors = [
+    "black",
+    "brown",
+    "red",
+    "orange",
+    "yellow",
+    "green",
+    "blue",
+    "violet",
+    "grey",
+    "white",
+];
 
+export function decodedValue(colors: Array<string>): number {
+    let sum = "";
+    colors.slice(0, 2).forEach((element) => {
+        sum += Colors.indexOf(element);
+    });
 
-
-export function decodedValue(colors:Array<string>) {
-  throw new Error('Remove this statement and implement this function')
+    return parseInt(sum);
 }
