b3933765282e928ab0a0be5890d5442facfb9bf5
YasinVeliyev <veliyev.yasin@gmail.com>
2022-11-27 18:39:48 +0400

    Rust

--- rust/pythagorean-triplet/src/lib.rs
+++ rust/pythagorean-triplet/src/lib.rs
@@ -2,7 +2,7 @@ use std::collections::HashSet;
 
 pub fn find(sum: u32) -> HashSet<[u32; 3]> {
     let mut set = HashSet::new();
-    for a in (1..sum / 3).rev() {
+    for a in 1..sum / 3 {
         let b = (sum.pow(2) - 2 * sum * a) / (2 * (sum - a));
         let c = sum - a - b;
         if a.pow(2) + b.pow(2) == c.pow(2) {
